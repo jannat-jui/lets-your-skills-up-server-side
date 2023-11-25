7017a70129ec25741f92850253f9f1a0e572abe3
jannat-jui <jannat15-13735@diu.edu.bd>
2023-11-25 10:24:13 -0800

    create get api and patch api for user

--- index.js
+++ index.js
@@ -90,6 +90,24 @@ async function run() {
       const result = await usersCollection.insertOne(user)
       res.send(result)
     })
+    app.get('/users', async (req, res) => {
+      console.log(req.headers)
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id= req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+
+    })
 
 
 
